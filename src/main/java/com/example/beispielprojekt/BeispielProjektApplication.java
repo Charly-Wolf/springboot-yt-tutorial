cbabaa92e781ebf26f44fff85e0a6df0fa7eb727
Carlos Paredes <cardan1990@hotmail.com>
2023-10-10 11:22:48 +0200

    test dependency injection

--- src/main/java/com/example/beispielprojekt/BeispielProjektApplication.java
+++ src/main/java/com/example/beispielprojekt/BeispielProjektApplication.java
@@ -2,12 +2,28 @@ package com.example.beispielprojekt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class BeispielProjektApplication {
 
+	public interface SaySomethingSevice {
+		public String saySomething();
+	}
+
+	@Component
+	public class SayHelloService implements SaySomethingSevice {
+		@Override
+		public String saySomething() {
+			return "Hello World";
+		}
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(BeispielProjektApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(BeispielProjektApplication.class, args);
+		SaySomethingSevice saySomethingSevice = applicationContext.getBean(SaySomethingSevice.class);
+		System.out.println(saySomethingSevice.saySomething());
 	}
 
 }
