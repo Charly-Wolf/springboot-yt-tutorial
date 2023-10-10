27470cb2fae065cfba743770093fb73cfaa6fe0f
Carlos Paredes <cardan1990@hotmail.com>
2023-10-10 11:31:24 +0200

    tested another way to use dependency injection

--- src/main/java/com/example/beispielprojekt/BeispielProjektApplication.java
+++ src/main/java/com/example/beispielprojekt/BeispielProjektApplication.java
@@ -3,6 +3,8 @@ package com.example.beispielprojekt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
@@ -12,11 +14,38 @@ public class BeispielProjektApplication {
 		public String saySomething();
 	}
 
-	@Component
-	public class SayHelloService implements SaySomethingSevice {
+//	@Component
+//	public class SayHelloService implements SaySomethingSevice {
+//		@Override
+//		public String saySomething() {
+//			return "Hello World";
+//		}
+//	}
+
+	@Configuration
+	public class SaySomethingConfiguration {
+		@Bean
+		public SaySomethingConfigurableService saySomethingConfigurableService() {
+			SaySomethingConfigurableService saySomethingConfigurableService = new SaySomethingConfigurableService();
+			saySomethingConfigurableService.setWhatToSay("Goodbye");
+			return saySomethingConfigurableService;
+		}
+	}
+
+	public class SaySomethingConfigurableService implements SaySomethingSevice {
+		private String whatToSay = "";
+
 		@Override
 		public String saySomething() {
-			return "Hello World";
+			return whatToSay;
+		}
+
+		public String getWhatToSay() {
+			return whatToSay;
+		}
+
+		public void setWhatToSay(String whatToSay) {
+			this.whatToSay = whatToSay;
 		}
 	}
 
